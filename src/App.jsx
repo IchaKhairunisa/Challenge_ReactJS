75ac13330cf58470a2218269b2cea08c8def4222
IchaKhairunisa <khairunisalsabilakurniawan@gmail.com>
2024-11-17 18:33:29 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProductPage from "./pages/Product/ProductPage"
+
+function App() {
+  
+  return (
+    <>
+     <ProductPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,35 @@
+export default function getAllProducts() {
+    return [
+      {
+        id: 'E-ESP',
+        slug: 'espresso-espresso',
+        name: "Espresso",
+        category: 'Espresso',
+        price: 10_000,
+        stock: 100,
+        imageUrl: 'public/assets/images/Espresso.png',
+        description: `The radiance lives on in the Nike Air Force 1 '07, the b-ball icon that puts a fresh spin on what you know best: crisp leather, bold colours and the perfect amount of flash to make you shine.`
+      },
+      {
+        id: 'IA-ESP',
+        slug: 'iced-americano-espresso',
+        name: "Iced Americano",
+        category: 'Espresso',
+        price: 18_000,
+        stock: 10,
+        imageUrl: `public/assets/images/IcedAmericano.png`,
+        description: `Comfortable, durable and timeless—it's number 1 for a reason. The classic '80s construction pairs with bold details for style that tracks whether you're on court or on the go.`
+      },
+      {
+        id: 'E-ESP',
+        slug: 'espresso-espresso',
+        name: "Espresso",
+        category: 'Espresso',
+        price: 10_000,
+        stock: 0,
+        imageUrl: 'public/assets/images/Espresso.png',
+        description: `The radiance lives on in the Nike Air Force 1 '07, the b-ball icon that puts a fresh spin on what you know best: crisp leather, bold colours and the perfect amount of flash to make you shine.`
+      },
+  
+    ]
+  }
